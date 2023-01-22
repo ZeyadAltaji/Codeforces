7ac7c1ce392fb7d0033eb22a1573bbf8b6a46b15
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-01-23 02:25:56 +0300

    Soldier and Bananas problem

---
+++ Soldier and Bananas/Soldier and Bananas.cpp
@@ -0,0 +1,24 @@
+// Soldier and Bananas.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int k, n, w;
+	int  total = 0;
+	cin >> k >> n >> w;
+	for (int i = 1; i <= w; i++) {
+		total = total + i * k;
+	}
+		if (total > n) {
+			cout << total - n;
+		}
+		else
+		{
+			cout << 0;
+		}
+	
+}
+ 
