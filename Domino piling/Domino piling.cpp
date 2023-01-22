0cd80f68676643aab863d964d87af113afd93aa1
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-01-23 01:38:56 +0300

    Domino piling problem

---
+++ Domino piling/Domino piling.cpp
@@ -0,0 +1,13 @@
+// Domino piling.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int M, N;
+	cin >> M >> N;
+	double square = (M * N) / (2 * 1);
+	cout << square << endl;
+
+}
